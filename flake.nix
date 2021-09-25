7ff07bb10b80dd1363c0f0ef5cb3c748efea6ade
Mate Hajzso <ajz@lambd.ax>
2021-09-25 02:01:56 +0200

    removes *-darwin systems from build

--- flake.nix
+++ flake.nix
@@ -11,32 +11,38 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, mach-nix-src }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let
-          pkgs = import nixpkgs { inherit system; };
+    let
+      lib = nixpkgs.lib;
+      defaultSystems = flake-utils.lib.defaultSystems;
+      buildSystems = lib.lists.subtractLists
+        [ "aarch64-darwin" "x86_64-darwin" ] defaultSystems;
+    in
+      flake-utils.lib.eachDefaultSystem (
+        system:
+          let
+            pkgs = import nixpkgs { inherit system; };
 
-          mach-nix = import mach-nix-src { inherit pkgs; python = "python38"; };
+            mach-nix = import mach-nix-src { inherit pkgs; python = "python38"; };
 
-          pyEnv = mach-nix.mkPython rec {
-            requirements = ''
-              requests
-              github3.py
-              xmltodict
-              pyasn
-              appdirs
-              pyyaml
-              androguard
-            '';
-          };
-        in
-          {
-            # defaultPackage = self.packages.${system}.${pname};
-            packages = { inherit pkgs; };
-            devShell = mach-nix.nixpkgs.mkShell {
-              # TODO add axml2xml go package as buildInput
-              buildInputs = with pkgs; [ pyEnv fdroidserver jdk11_headless wget curl ];
+            pyEnv = mach-nix.mkPython rec {
+              requirements = ''
+                requests
+                github3.py
+                xmltodict
+                pyasn
+                appdirs
+                pyyaml
+                androguard
+              '';
             };
-          }
-    );
+          in
+            {
+              # defaultPackage = self.packages.${system}.${pname};
+              packages = { inherit pkgs; };
+              devShell = mach-nix.nixpkgs.mkShell {
+                # TODO add axml2xml go package as buildInput
+                buildInputs = with pkgs; [ pyEnv fdroidserver jdk11_headless wget curl ];
+              };
+            }
+      );
 }
